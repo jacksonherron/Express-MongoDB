18c2955da2dfa74d482ce88b71cb06e0a48b4f11
jacksonherron <jacksonherron@gmail.com>
2019-07-15 18:08:49 -0700

    Refactored code to vanilla JS

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,150 +1,158 @@
+// import { STATES } from "mongoose";
+
 // ------------------ GLOBAL VARIABLES ------------------ //
 const BASE_URL = '/api/v1/cities';
 
-// ------------------ DOM ELEMENTS ------------------ //
-const $newCityForm = $('#newCityForm');
-const $cities = $('#cities')
+// ------------------ STATE VARIABLES ------------------ //
 
+const state = {
+    cities: []
+}
+
+// ------------------ DOM ELEMENTS ------------------ //
+const newCityForm = document.querySelector('#newCityForm');
+const citiesSection = document.querySelector('#cities');
 
 
 // ------------------ FUNCTIONS ------------------ //
 
+// ------------------ READ ------------------ //
+const getAllCities = () => {
+    fetch(BASE_URL)
+        .then(res => res.json())
+        .then(json => {
+            state.cities = json.data;
+            console.log(state.cities);
+            render(state.cities);
+        })
+        .catch(err => console.log(err));
+};
+
+
+const render = (arr) => {
+    citiesSection.innerHTML = '';
+    arr.forEach(city => {
+        const template = cityTemplate(city);
+        citiesSection.insertAdjacentHTML('beforeend', template)
+    })
+}
 
 // ------------------ CREATE ------------------ //
 const addNewCity = (event) => {
     event.preventDefault();
-    $.ajax({
+    const name = document.getElementById('name');
+    const description = document.getElementById('description');
+    const newCity = { name: name.value, description: description.value };
+    fetch(BASE_URL, {
         method: 'POST',
-        url: BASE_URL,
-        data: $newCityForm.serialize(),
-        success: newCitySuccess,
-        error: newCityError
-    });
-};
-
-const newCitySuccess = (response) => {
-    console.log('Success');
-    $cities.append(cityTemplate(response.data));
-};
-
-const newCityError = (err1, err2, err3) => {
-    alert(err2);
-};
-
-
-// ------------------ READ ------------------ //
-const getAllCities = () => {
-    $.ajax({
-        method: 'GET',
-        url: BASE_URL,
-        success: allCitiesSuccess,
-        error: allCitiesError
-    });
-};
-
-const allCitiesSuccess = (response) => {
-    console.log('Success')
-    response.data.forEach(city => $cities.append(cityTemplate(city)));
-};
-
-const allCitiesError = (err1, err2, err3) => {
-    alert(err2);
-};
-
+        body: JSON.stringify(newCity),
+        headers:{ 'Content-Type': 'application/json' }
+        })
+        .then(res => res.json())
+        .then(json => { 
+            citiesSection.insertAdjacentHTML('beforeend', cityTemplate(json.data));
+            state.cities.push(json.data);
+            console.log(state.cities);
+        })
+        .catch(err => console.log(err));
+}
 
 // ------------------ UPDATE ------------------ //
 const editCity = (event) => {
-    // First step: Deactivate the edit button so it can't be clicked more than once
     event.preventDefault();
-    $('.editBtn').prop('disabled', true);
-    // $(event.target).prop('disabled', true);
-    $(editTemplate()).insertAfter(event.target.parentNode.parentNode);
-    const $editCityForm = $(event.target.parentNode.parentNode.nextSibling.childNodes[1]);
-    const $cancelEditButton = $(event.target.parentNode.parentNode.nextSibling.childNodes[3]);
-    const $editDiv = $(event.target.parentNode.parentNode.nextSibling);
-
-    $cancelEditButton.on('click', (editButton) => {
-        event.preventDefault();
-        $('.editBtn').prop('disabled', false);
-        // $(event.target.parentNode.childNodes[3]).prop('disabled', false);
-        $editDiv.remove();
+    event.target.disabled = true;
+    const cityToEdit = event.target.parentNode.parentNode;
+    cityToEdit.insertAdjacentHTML('afterend', editTemplate());
+    const editDiv = event.target.parentNode.parentNode.nextSibling;
+    const editForm = event.target.parentNode.parentNode.nextSibling.childNodes[1];
+    const cancelEditButton = event.target.parentNode.parentNode.nextSibling.childNodes[3];
+    // Add cancel edit button listener
+    cancelEditButton.addEventListener('click', (event2) => {
+        event2.preventDefault();
+        event.target.disabled = false;
+        editDiv.parentNode.removeChild(editDiv);
     });
-
-    $editCityForm.on('submit', () => {
-        event.preventDefault();
-        $.ajax({
+    // Add sumbit edit button listener
+    editForm.addEventListener('submit', (event3) => {
+        event3.preventDefault();
+        const editedCity = { name: editForm.childNodes[1].value, description: editForm.childNodes[3].value };
+        fetch(`${BASE_URL}/${cityToEdit.id}`, {
             method: 'PUT',
-            url: `${BASE_URL}/${event.target.parentNode.parentNode.id}`,
-            data: $editCityForm.serialize(),
-            success: editCitySuccess,
-            error: editCityError
-        });
+            body: JSON.stringify(editedCity),
+            headers:{ 'Content-Type': 'application/json' }
+            })
+            .then(res => res.json())
+            .then(json => { 
+                cityToEdit.insertAdjacentHTML('beforebegin', cityTemplate(json.data));
+                cityToEdit.parentNode.removeChild(cityToEdit);
+                editForm.parentNode.parentNode.removeChild(editForm.parentNode);
+                const index = state.cities.findIndex((city) => {
+                    return city._id === json.data._id;
+                });
+                state.cities[index] = json.data;
+                console.log(state.cities);
+            })
+            .catch(err => console.log(err));
     });
-};
-
-const editCityError = (err1, err2, err3) => {
-    alert(err2);
-};
-
-const editCitySuccess = (response) => {
-    console.log('Success');
 };
 
 
 // ------------------ DESTROY ------------------ //
 const deleteCity = (event) => {
     event.preventDefault();
-    let id = event.target.parentNode.parentNode.id;
-    $.ajax({
-        method: 'DELETE',
-        url: `/api/v1/cities/delete/${id}`,
-        success: deleteCitySuccess,
-        error: deleteCityError
-    });
+    let cityId = event.target.parentNode.parentNode.id;
+
+    fetch(`${BASE_URL}/delete/${cityId}`, { method: 'delete' })
+        .then(res => res.json())
+        .then(json => {
+            const id = json.data._id
+            const city = document.getElementById(id);
+            if (city.nextSibling.classList.contains('edit')) city.parentNode.removeChild(city.nextSibling);
+            city.parentNode.removeChild(city);
+            const index = state.cities.findIndex((city) => {
+                return city._id === id;
+            });
+            state.cities.splice(index, 1);
+            console.log(state.cities);
+        })
+        .catch(err => console.log(err));
 };
 
-const deleteCityError = (err1, err2, err3) => {
-    alert(err2);
-};
 
-const deleteCitySuccess = (response) => {
-    console.log('Success');
-    let id = response.data._id;
-    let $city = $(`#${id}`);
-    $city.remove();
-};
 
-const editTemplate = () => {
-    return `
-    <div class="edit">
-        <form class="editCityForm">
-            <input type="text" id="name" name="name" placeholder="Name" class="input"/>
-            <input type="text" id="description" name="description" placeholder="Description" class="input"/>
-            <button type="submit" class="submitEditBtn">Edit City</button>
-        </form>
-        <button type="submit" class="cancelEditBtn">&times;</button>
-    </div>`;
-};
-
+// ------------------ HTML TEMPLATES ------------------ //
 const cityTemplate = (city) => {
-    return `
-    <div id="${city._id}" class="city">
+    return `<div id="${city._id}" class="city">
         <div>
-            <h4>${city.name}</h4>
-            <p>${city.description}</p>
+        <h4>${city.name}</h4>
+        <p>${city.description}</p>
         </div>
         <div class="buttons">
             <button class="deleteBtn">&times;</button>
             <button class="editBtn">edit</button>
         </div>
-    </div>`;
+    </div>`
+};
+
+const editTemplate = () => {
+    return `<div class="edit">
+        <form class="editForm">
+            <input type="text" id="name" name="name" placeholder="Name" class="input"/>
+            <input type="text" id="description" name="description" placeholder="Description" class="input"/>
+            <button type="submit" class="submitEditBtn">Edit City</button>
+        </form>
+        <button type="submit" class="cancelEditBtn">&times;</button>
+    </div>`
 };
 
 // ------------------ Event Listeners ------------------ //
-$newCityForm.on('submit', addNewCity);
-$cities.on('click', '.deleteBtn', deleteCity);
-$cities.on('click', '.editBtn', editCity);
+newCityForm.addEventListener('submit', addNewCity);
+
+citiesSection.addEventListener('click', () => {
+    if (event.target.classList.contains('deleteBtn')) deleteCity(event);
+    if (event.target.classList.contains('editBtn')) editCity(event);
+});
 
 
-// Append cities to DOM
+// Append cities to DOM when web page opens
 getAllCities();
